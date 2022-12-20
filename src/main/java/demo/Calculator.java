6f48fb349c3834106172394c467c6ac252204eba
Dwarkesh <dwarkeshprashantmore@gmail.com>
2022-12-20 14:39:27 +0530

    Initial Commit

--- src/main/java/demo/Calculator.java
+++ src/main/java/demo/Calculator.java
@@ -1,10 +1,24 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Calculator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first no. :");
+		
+		int a  = sc.nextInt();
+		
+		System.out.println("Enter second no. :");
+		
+		int b = sc.nextInt();
+		
+		System.out.println("Addition :" + Maths.add(a,b));
+		sc.close();
+		
 	}
 
 }
--- src/main/java/demo/Maths.java
+++ src/main/java/demo/Maths.java
@@ -1,5 +1,17 @@
 package demo;
 
 public class Maths {
+	
+	public static int add(int a , int b)
+	{
+		int c = a + b;
+		return c;
+	}
+	public static int multiply(int a , int b)
+	{
+		int c = a * b;
+		return c;
+	}
+	
 
 }
