2910ab6fe83ea2a8ccdb7558261bb5ecdad0bb8a
Dwarkesh <dwarkeshprashantmore@gmail.com>
2022-12-20 15:16:13 +0530

    Revert "added overloaded method"
    This reverts commit eb0708f1dfbd8a4c38c0cf40c9c3dc16b135d893.

--- src/main/java/demo/Maths.java
+++ src/main/java/demo/Maths.java
@@ -7,16 +7,6 @@ public class Maths {
 		int c = a + b;
 		return c;
 	}
-	
-	public static int add(int a , int b ,int c)
-	{
-		int d = a + b  + c;
-		return d;
-	}
-	
-	
-	
-	
 	public static int multiply(int a , int b)
 	{
 		int c = a * b;
